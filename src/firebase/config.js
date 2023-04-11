7f568507a6571b11f204ee3c004f730cb4cc30be
Prajjwal Bisht <112104242+theanonymouspahadi@users.noreply.github.com>
2023-04-11 12:01:59 +0530

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTNYgMWBS7jlkub6pSn9_7YMApiir5lGA",
+    authDomain: "linkedin-clone-b9705.firebaseapp.com",
+    projectId: "linkedin-clone-b9705",
+    storageBucket: "linkedin-clone-b9705.appspot.com",
+    messagingSenderId: "169253083148",
+    appId: "1:169253083148:web:86fdaa88c76f1d91ecb629"
+  };
+
+  export default firebaseConfig;
